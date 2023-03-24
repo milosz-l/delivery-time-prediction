5a91f213f5d1342a29c186180e6c66d96c3d8f7a
Milosz Lopatto <milosz.lopatto@gmail.com>
2023-03-24 19:00:29 +0100

    moved TimeDiffConstants to config.py

--- src/config.py
+++ src/config.py
@@ -4,6 +4,25 @@ create Pydantic models
 from typing import List
 
 from pydantic import BaseModel, validator
+from random import randint
+
+
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
+PRICE_TRESHOLD = 100_000    # for outliers
+WEIGHT_TRESHOLD = 50        # for outliers
+NUM_OF_HOURS = 24
+SEED = randint(0, 100000)  # 42
+TEST_SIZE = 0.001
+
+COLS_TO_DROP_ALWAYS = ["delivery_timestamp",
+                       "session_id",
+                       "purchase_id",
+                       "event_type",
+                       "name",
+                       "user_id",
+                       'offered_discount',
+                       'optional_attributes',
+                       'purchase_timestamp']
 
 
 def must_be_non_negative(v: float) -> float:
